14e5321e13c0c965e9792f7bdb08c320a820c968
NeuronaBens <71675911+NeuronaBens@users.noreply.github.com>
2021-11-11 21:11:46 -0500

    Update Hito3.py

--- SegundaEntrega/Hito3.py
+++ SegundaEntrega/Hito3.py
@@ -33,6 +33,25 @@ def dijkstra(G, s):
 
   return path, cost
 
+def mapRealToSuplementaryNodeIndexes(realGraph, artMaxV, minValue, hInc):
+    #returns a suplementaryGraph
+    supG = []
+    
+    for node in realGraph:
+        supG.append([])
+        for pair in node:
+            
+            a = pair[0] - minValue
+            
+            val = a - int(a/hInc)*hInc + int(a/hInc)*artMaxV
+            
+            newPair = (val, pair[1])
+            supG[-1].append(newPair)
+    return supG
+
+def mapSuplementaryToRealNodeIndexes(realList, suplementaryGraph):
+    return None
+
 def createRectangularSubGraph(G, center, toSideSize, maxH, maxV):
     x, y = uf.findCoordinatesPerN(center, maxV)
     minX = x - toSideSize
@@ -54,13 +73,19 @@ def createRectangularSubGraph(G, center, toSideSize, maxH, maxV):
     maxY = int(maxY)
     minY = int(minY)
     
+    nc = None
+    c = 0
+    
     indexes = []
     for i in range(minX, maxX+1):
         for j in range(minY, maxY+1):
             indexes.append(uf.findNPerCoordinates(i, j, maxV, maxH))
+            if indexes[-1] == center:
+                nc = c
+            c += 1
     
     newG = []
-    for i in indexes:
+    for i in indexes: ########################################iasdisasda
         newG.append(G[i])
         j = 0
         for conec in newG[-1]:
@@ -68,31 +93,40 @@ def createRectangularSubGraph(G, center, toSideSize, maxH, maxV):
                 newG[-1].pop(j)
             j+=1
     
-    return newG, indexes
+    i = 0 ##█##█##█##█##█##█##█##█##█
+    for node in newG:
+        j = 0
+        for conec in node:
+            if conec[0] < indexes[0]:
+                print(newG[i].pop(j))
+            j += 1
+        i += 1 ##█##█##█##█##█##█##█##█##█
     
-
-print(createRectangularSubGraph(g, 1001, 5, 1000, 1000))
-
+    artMaxH = len(range(minX, maxX+1))
+    artMaxV = len(range(minY, maxY+1))
+    
+    return [newG, indexes, nc, artMaxH, artMaxV]
 
 ###########
-#BFS partiendo desde los puntos de almacén
+#Dijkstra partiendo desde los puntos de almacén en un subgrafo
 #types contiene los tipos de puntos siendo 499750 vacíos (n), 499750 puntos de entrega (e) y 500 puntos de almacén (d)
 #Se recorrerá types para identificar los index de los nodos en los que sean de almacén y luego con esto realizar el BFS partiendo desde este punto
 
 index = 0
 for i in types:
   if i == 'd':
-    newg = createRectangularSubGraph(g, i, 5, 1000, 1000) #5 de distancia a los lados de cada punto de distribución 
-    path = dijkstra(newg, i)
+    newg = createRectangularSubGraph(g, index, 5, 1000, 1000) #5 de distancia a los lados de cada punto de distribución 
+    
+    newg[0] = mapRealToSuplementaryNodeIndexes(newg[0], newg[-1], newg[1][0], 1000)
+    
+    path = dijkstra(newg[0], newg[2])
     print(path)
+    
   index = index + 1
 
 
-
-
-
-
-
+###ten en cuenta que se esta entregando el path en función de su posición relativa
+###tienes que hacer el map suplentary to real para conseguir el camino real.
 
 
 
